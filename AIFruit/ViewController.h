c6cfea7b9af035bcf4ffaf9fdbc3121fa89a503e
Kosne <804063213@qq.com>
2016-03-14 10:43:00 +0800

    Initial Commit

---
+++ AIFruit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AIFruit
+//
+//  Created by Kosne on 16/3/14.
+//  Copyright © 2016年 Kosne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AIFruit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AIFruit
+//
+//  Created by Kosne on 16/3/14.
+//  Copyright © 2016年 Kosne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
